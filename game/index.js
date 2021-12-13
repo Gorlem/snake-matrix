d167e18c88a112da20293885b7daf98b0f26e138
Dennis Dörr <dev@ddoerr.com>
2021-12-13 15:06:54 +0100

    renamed message names and combined board and extras back into one message

--- game/index.js
+++ game/index.js
@@ -15,35 +15,25 @@ client.on('error', (err) => {
 
 client.on('connect', () => {
     console.log(`Connected to ${ip}`);
-    client.subscribe('game/snake/direction');
-    client.subscribe('game/snake/action');
+    client.subscribe('direction');
+    client.subscribe('action');
 });
-
-
-let nextDirection = Direction.Right;
 
 const game = new Game(8, 8);
 
 client.on('message', (topic, payload) => {
     console.log(topic);
-    if (topic === 'game/snake/direction') {
+    if (topic === 'direction') {
         game.move(Direction.fromString(payload.toString()))
     }
-    if (topic === 'game/snake/action' ) {
+    if (topic === 'action' ) {
         game.action();
     }
 });
 
-console.log(game.getBoard());
-
 setInterval(() => {
     game.tick();
-    console.log(game.getBoard());
-    
     const board = game.getBoard();
-    client.publish('game/snake/board', board);
-
     const extras = game.getExtras().flat();
-    console.log(extras);
-    client.publish('game/snake/extras', Buffer.from(extras));
+    client.publish('board', board.concat(Buffer.from(extras)));
 }, 1000);
